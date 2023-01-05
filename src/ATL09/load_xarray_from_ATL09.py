fb4d89a0b5f4750120a4f46c73cc77cd2c2a0fc8
Andrew Martin <eeasm@host596.jc.rl.ac.uk>
2023-01-05 15:17:25 +0000

    implemented load_xarray_from_ATL09_icepyx function with hacked version of icepyx

--- src/ATL09/load_xarray_from_ATL09.py
+++ src/ATL09/load_xarray_from_ATL09.py
@@ -20,7 +20,7 @@ def load_xarray_from_ATL09(filename,subsetVariables=None):
     '''
     pass
 
-def load_xarray_from_ATL09_icepyx(filename):
+def load_xarray_from_ATL09_icepyx(filename, pattern=None, wanted_vars=None):
     '''Function to load in ATL09 datafiles to xarray format, utilising the icepyx.Read object.
 
     Given the limitations of icepyx release 0.6.4 (see Github issue #397) I've edited the icesat_summit version of icepyx.Read to allow ATL09 to be laoded.
@@ -32,3 +32,16 @@ def load_xarray_from_ATL09_icepyx(filename):
         5: " "_2/" "
         6: " "_3/" "
     '''
+    if pattern is None:
+        pattern = "processed_ATL{product:2}_{datetime:%Y%m%d%H%M%S}_{rgt:4}{cycle:2}{orbitsegment:2}_{version:3}_{revision:2}.h5"
+    product = 'ATL09'
+
+    reader = ipx.Read(data_source=filename, product=product, filename_pattern=pattern)
+
+    # add the desired variables
+    if wanted_vars is None:
+        wanted_vars = ['longitude','latitude','cab_prof','density_pass2']
+    reader.vars.append(var_list=wanted_vars)
+
+    ds = reader.load()
+    return ds
