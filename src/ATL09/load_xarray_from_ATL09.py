7c9fb1581180136a003304a28b34feba23756e3b
Andrew Martin <eeasm@host596.jc.rl.ac.uk>
2023-01-05 13:36:55 +0000

    Tests for icepyx .Read Method. \nIve found that the icepyx module doesnt work with ATL09 data, as it looks for spots rather than profiles. \nIve come up with a hack solution to this and raised issue #397 on the icepyx Github. However, going forward, think Ill create my own solution to loading in the data.

---
+++ src/ATL09/load_xarray_from_ATL09.py
@@ -0,0 +1,34 @@
+import os
+import h5py as h5
+import xarray as xr
+import icepyx as ipx
+
+def load_xarray_from_ATL09(filename,subsetVariables=None):
+    '''Function to load in ATL09 data to xarray format from the hdf5 file format.
+    
+    The function will first open the h5 file and then create a high-frequency and low-frequency xr.Dataset objects.
+    Each xr.Dataset will have the dimensions of:
+        + profile = 1,2,3
+        + time = ['bkgrd_atlas']['delta_time']
+        + height = ['profile1']['ds_va_bin_h']
+        + layer = [0:10]
+
+    I will then create xr.DataArray objects for datastructure in the profiles, with dimensions of profile,time and (height/layer) if required.
+    These will then be placed into the xr.Dataset objects.
+
+    At the end, I will return both the high-frequency and low-frequency datasets. These could then possibly be conjoined afterwards along the time axis, although I'm unsure if thats a good idea or not...
+    '''
+    pass
+
+def load_xarray_from_ATL09_icepyx(filename):
+    '''Function to load in ATL09 datafiles to xarray format, utilising the icepyx.Read object.
+
+    Given the limitations of icepyx release 0.6.4 (see Github issue #397) I've edited the icesat_summit version of icepyx.Read to allow ATL09 to be laoded.
+    In this situation, each profile is assigned to a 'spot':
+        1: profile_1/high_rate
+        2: " "_2/" "
+        3: " "_3/" "
+        4: profile_1/low_rate
+        5: " "_2/" "
+        6: " "_3/" "
+    '''
