46ff120ebdf7bce283d4c5cf95d965be60122d58
Andrew Martin <eeasm@host596.jc.rl.ac.uk>
2023-01-19 14:51:52 +0000

    added Sarahs function read_radar_data to load_xarray_from_mmcrmom

--- src/mmcr/load_xarray_from_mmcrmom.py
+++ src/mmcr/load_xarray_from_mmcrmom.py
@@ -7,6 +7,7 @@ Code to load in the .nc files from a folder and concatenate them into a singular
 import os
 import xarray as xr
 import numpy as np
+import netCDF4
 
 def load_xarray_from_mmcrmom(dir_target):
     '''Function to load multiple MMCRMOM.nc files in dir_target into a singular xr.Dataset object.
@@ -40,3 +41,48 @@ target = '/home/users/eeasm/_scripts/ICESat2/src/mmcr'
 ds = load_xarray_from_mmcrmom(target)
 print(ds)
 '''
+
+def read_radar_data(start_date,stop_date,mode_idx, mask_sn): 
+    '''Author: Sarah Barr
+    Creation date: 18/1/23
+
+    Function to read processed radar files between two dates, select data from one mode and output one xarray dataset
+    
+    Input: 
+     - dates: in format YYYY-MM-DD
+     - mask_sn: boolean, True masks data where signal to noise ratio <-14
+    '''
+    #start_time = time.time()
+    dpath = '/gws/nopw/j04/ncas_radar_vol2/data/ICECAPSarchive/mmcr/processed/'
+    
+    all_files = []
+    date_list = pd.date_range(start_date,stop_date,freq='1D')
+
+    for date in date_list:
+        all_files.append(glob.glob(dpath+ 'smtmmcrmomX1.b1.%s*.cdf'%dt.datetime.strftime(date,format='%Y%m%d')))
+
+    flatten = lambda l: [item for sublist in l for item in sublist]
+    all_files = flatten(all_files)
+    all_files.sort()
+
+    # get heights to add back later (necessary due to problem with xarray trying to input netcdf with a variable and dimension called 'heights')
+    mmcr_heights = netCDF4.Dataset(all_files[0],'r')
+    height_vals = mmcr_heights.variables['heights'][:]
+
+    mmcr_data = xr.open_mfdataset(all_files,concat_dim = 'time', combine = 'nested',drop_variables='heights')
+    
+    #reassign height values 
+    mmcr_data = mmcr_data.assign(height_vals=(['mode','heights'],height_vals))
+    
+    # select data from chosen mode
+    mmcr_data = mmcr_data.sel(mode = mode_idx)
+    mmcr_data = mmcr_data.where(mmcr_data.ModeNum==3., drop = True)
+    
+    mmcr_data = mmcr_data.assign_coords(time =mmcr_data.time_offset)
+    mmcr_data = mmcr_data.assign_coords(heights =mmcr_data.height_vals[:,0].values)
+    
+    if mask_sn:
+        mmcr_data = mmcr_data.where(mmcr_data.SignalToNoiseRatio>-14)
+        
+    #print("--- %s seconds ---" % (time.time() - start_time))
+    return mmcr_data
