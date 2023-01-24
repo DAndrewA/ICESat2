069237b22c51a07e955d31dbb4880e4631e8a828
Andrew Martin <eeasm@host596.jc.rl.ac.uk>
2023-01-24 15:51:37 +0000

    written and tested extract_from_mplgz: working

---
+++ src/mpl/extract_from_mplgz.py
@@ -0,0 +1,49 @@
+'''Author: Andrew Martin
+Creation date: 24/01/2023
+
+Function to extract the .mpl.gz files from dir_target/mplraw_zip to dir_target/mplraw
+'''
+
+import gzip
+import os
+import shutil
+import warnings
+
+def extract_from_mplgz(dir_target):
+    '''Function to extract the .mpl.gz files from dir_target/mplraw_zip into dir_target/mpl_raw
+    
+    INPUTS:
+        dir_target : string
+            Target directory that contains the /mplraw_zip folder containing .mpl.gz files.
+    '''
+    dir_zip = os.path.join(dir_target, 'mplraw_zip')
+    dir_unzip = os.path.join(dir_target, 'mplraw')
+
+    # create the dir_target/mplraw folder if it doesn't already exist
+    if not os.path.isdir(dir_unzip):
+        warnings.warn(f'extract_from_mplgz: /mplraw does not exist at {dir_target=}. Creating subdirectory now.')
+        os.mkdir(dir_unzip)
+
+    files_mpl = os.listdir(dir_zip)
+    files_mpl = [f for f in files_mpl if f[-7:] == '.mpl.gz']
+
+    files_unzip = os.listdir(dir_unzip)
+
+    for fname in files_mpl:
+        if fname[:-3] in files_unzip:
+            # if the file has already been extracted, pass
+            print(f'{fname} : already extracted')
+            continue
+        print(f'{fname} : extracting')
+        
+        # solution taken from https://stackoverflow.com/questions/31028815/how-to-unzip-gz-file-using-python
+        # Matt & Erick 's solutions
+        with gzip.open(os.path.join(dir_zip,fname), 'rb') as f_in:
+            with open(os.path.join(dir_unzip, fname[:-3]), 'wb') as f_out:
+                shutil.copyfileobj(f_in,f_out)
+
+    return None
+
+# example code
+target = '/home/users/eeasm/_scripts/ICESat2/src/mpl'
+extract_from_mplgz(target)
