60328e34a26b767c68e8dc11b4a4d0bb4dcb2ff2
Andrew Martin <eeasm@host596.jc.rl.ac.uk>
2023-05-05 19:08:37 +0100

    dda: implemented convolve_masked function, requires testing

--- src/eeasm_icesat/dda/dda.py
+++ src/eeasm_icesat/dda/dda.py
@@ -3,3 +3,39 @@ Creation date: 05/05/2023
 
 Scripts for implementing the dda-atmos cloud detection algorithm on backscatter data.
 '''
+
+import numpy as np
+from scipy import signal
+
+def convolve_masked(data, mask, kernal, **kwargs):
+    '''Function to perform a convolution of a kernal on masked data.
+    
+    The convention is that masked values are 1s in mask, and the data to convolve is 0s in the mask.
+    For a convolution on masked data, this is essentially treating the nan values as 0, but also calculating the changed kernal normalisation due to the mask.
+
+    INPUTS:
+        data : np.ndarray
+            array containing the data for which we want to perform the masked convolution on
+
+        mask : np.ndarray (dtype=boolean)
+            array of the same shape as data that contains 1s where nans and masked values are, and 0s where the desired data exists.
+
+        kernal : np.ndarray
+            convolutional kernal
+
+        kwargs : other arguments to be passed to np.convolve
+
+    OUTPUTS:
+
+    '''
+    convargs = {}
+    for arg in ['mode','boundary','fillvalue']:
+        if arg in kwargs:
+            convargs[arg] = kwargs[arg]
+
+    norm = signal.convolve2d(~mask, kernal, **convargs)
+    masked_data = data.copy()
+    masked_data[mask] = 0
+    density = signal.convolve2d(masked_data,kernal, **convargs)
+
+    return density, norm
