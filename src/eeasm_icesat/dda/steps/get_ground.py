30ed03168831e2a4ddb64a4c0056f38b0e7a816a
Andrew Martin <eeasm@host596.jc.rl.ac.uk>
2023-06-16 16:47:05 +0100

    dda: setting up for refactoring of dda module

---
+++ src/eeasm_icesat/dda/steps/get_ground.py
@@ -0,0 +1,5 @@
+'''Author: Andrew Martin
+Creation Date: 16/6/23
+
+Function to get the ground return bin in the data (if it exists) by checking bins in a density pass that occur within a tolerence of the DEM and are found in the cloud mask.
+'''
