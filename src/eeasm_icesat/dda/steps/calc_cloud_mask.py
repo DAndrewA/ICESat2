0861868e5fb79ad8e55d862834eb95f483bdac07
Andrew Martin <eeasm@host596.jc.rl.ac.uk>
2023-06-18 16:27:07 +0100

    dda:steps: implemented calc_cloud_mask.py

--- src/eeasm_icesat/dda/steps/calc_cloud_mask.py
+++ src/eeasm_icesat/dda/steps/calc_cloud_mask.py
@@ -3,3 +3,35 @@ Creation Date: 16/6/23
 
 Function to caluclate the cloud mask, given the density field, thresholds and data mask, with the option to remove small clusters.
 '''
+
+import numpy as np
+from skimage.morphology import remove_small_objects
+
+def calc_cloud_mask(density, threshold, data_mask=None, remove_small_clusters=0):
+    '''Function to calculate the cloud mask from a density field and the associated thresholds.
+    
+    INPUTS:
+        density : np.ndarray
+            nxm numpy array containing the density field
+
+        threshold : np.ndarray
+            (x,) numpy ndarray containing the threshold value for each vertical profile.
+
+        data_mask : np.ndarray (dtype=boolean), None
+            nxm numpy array determining which values in the input data are invalid. If not None, then regions of the cloud_mask will be set to 0 where data_mask is True. Otherwise, cloud_mask will not be affected.
+
+        remove_small_clusters : int
+            Variable denoting how big the minimum cluster size can be in the mask. If a connected cluster of masked pixels (clouds) are smaller than this value, they will be removed according to skimage.morphology.remove_small_objects. If 0, no clusters are removed.
+
+    OUTPUTS:
+        cloud_mask : np.ndarray (dtype=boolean)
+            nxm numpy array containing cloudy pixels (1s) and non-cloudy pixels (0s).
+    '''
+    cloud_mask = np.greater(density,threshold).astype(bool)
+    if data_mask is not None:
+        cloud_mask[data_mask] = 0
+
+    if remove_small_clusters > 0:
+        cloud_mask = remove_small_objects(cloud_mask,min_size=remove_small_clusters)
+
+    return cloud_mask
