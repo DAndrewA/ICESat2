9989f3dba87b8e7dc90ab9e510e3523594d4912e
Andrew Martin <114.amart@gmail.com>
2023-06-20 13:30:33 +0100

    dda:steps: updated init for newly implemented functions

--- src/eeasm_icesat/dda/steps/__init__.py
+++ src/eeasm_icesat/dda/steps/__init__.py
@@ -4,7 +4,7 @@ from .calc_threshold import calc_threshold
 from .combine_layers_from_mask import combine_layers_from_mask
 from .combine_layers_from_mask import combine_layers_from_mask_vectorized
 from .combine_masks import combine_masks
-#from .get_ground import get_ground
+from .get_ground_bin import get_ground_bin
 from .get_layer_boundaries import get_layer_boundaries
-#from .remove_ground_from_mask import remove_ground_from_mask
+from .remove_ground_from_mask import remove_ground_from_mask
 #from .run_pass import run_pass
