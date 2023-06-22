34630c183abdc7c45b69ea9bb0413002f36f42b3
Andrew Martin <eeasm@host596.jc.rl.ac.uk>
2023-06-22 18:21:13 +0100

    dda:steps: replace with noise unvectorized removed

--- src/eeasm_icesat/dda/steps/replace_mask_with_noise.py
+++ src/eeasm_icesat/dda/steps/replace_mask_with_noise.py
@@ -42,52 +42,6 @@ def replace_mask_with_noise(data, mask, mean, sd, vmin=0, seed=None, verbose=Tru
 
     noisy_data = data.copy()
     (n_prof, n_vert) = noisy_data.shape
-
-    for i, (p,m,s) in enumerate(zip(mask,mean,sd)):
-        num_rand = np.sum(p)
-        rand_vals = np.random.default_rng(seed).normal(loc=m,scale=s,size=num_rand)
-        rand_vals[rand_vals < vmin] = vmin
-        noisy_data[i, p] = rand_vals
-
-    return noisy_data
-
-
-def replace_mask_with_noise_vectorized(data, mask, mean, sd, vmin=0, seed=None, verbose=True):
-    '''Function to replace the values denoted by cloud_mask in data with normally distributed noise.
-
-    INPUTS:
-        data : np.ndarray
-            (n,m) numpy array containing the data field, which will be partially filled with noise.
-
-        mask : np.ndarray (dtype=bool)
-            (n,m) numpy array containing 1s where noise should be and 0s where the original data is to be kept.
-
-        mean : np.ndarray
-            (n,) numpy array of the noise spectrum means for each vertical profile.
-
-        sd : np.ndarray
-            (n,) numpy array containing the standard deviations of the noise spectra for each vertical profile.
-
-        vmin : float
-            Minimum value that the returned data will contain. This accounts for the fact that some noise values could fall below a physical threshold (i.e. 0).
-
-        seed : None, int
-            Value used to initialise the scipy.stats.rv_continuous.random_state object. None will result in randomness on each run, whereas a provided number will seed the random algorithm.
-
-        verbose : bool
-            Flag for printing debug statements to the output log.
-
-
-    OUTPUTS:
-        data : np.ndarray
-            (n,m) numpy array, consisting of the data variable with some values replaced by noise.
-    '''
-    if verbose:
-        print('==== dda.steps.replace_mask_with_noise()')
-        print(f'{seed=}')
-
-    noisy_data = data.copy()
-    (n_prof, n_vert) = noisy_data.shape
     rs = (n_prof,1)
 
     noise = np.random.default_rng(seed).normal(loc=mean.reshape(rs), scale=sd.reshape(rs), size=noisy_data.shape)
