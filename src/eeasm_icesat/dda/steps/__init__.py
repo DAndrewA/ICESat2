edf03ee16ef64ad66eecc9df360618742c55b160
Andrew Martin <eeasm@host596.jc.rl.ac.uk>
2023-06-22 17:45:24 +0100

    dda:steps: updated init

--- src/eeasm_icesat/dda/steps/__init__.py
+++ src/eeasm_icesat/dda/steps/__init__.py
@@ -3,10 +3,14 @@ from . import create_kernal
 from .calc_cloud_mask import calc_cloud_mask
 from .calc_density import calc_density
 from .calc_threshold import calc_threshold
+from .calc_threshold import calc_threshold_vectorized
 from .combine_layers_from_mask import combine_layers_from_mask
 from .combine_layers_from_mask import combine_layers_from_mask_vectorized
 from .combine_masks import combine_masks
 from .get_ground_bin import get_ground_bin
 from .get_layer_boundaries import get_layer_boundaries
 from .remove_ground_from_mask import remove_ground_from_mask
+
+from .calc_noise_at_height import calc_noise_at_height
+from .replace_mask_with_noise import replace_mask_with_noise
 #from .run_pass import run_pass
