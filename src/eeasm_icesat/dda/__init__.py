9377861f006db006f1b9a6e95aa95a53d3bc3d0b
Andrew Martin <DAndrewA@users.noreply.github.com>
2025-01-30 15:19:37 +0000

    removed dda: is seperate package

--- src/eeasm_icesat/dda/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import dda
-from .compute_cloud_layers import compute_cloud_layers
-from .dda_atl import dda_atl
-from . import steps
--- src/eeasm_icesat/dda/steps/run_pass.py
+++
@@ -1,5 +0,0 @@
-'''Author: Andrew Martin
-Creation Date: 16/6/23
-
-Function to implement the density caluclation, threshold calculation and cloud mask calculation into a single function.
-'''
